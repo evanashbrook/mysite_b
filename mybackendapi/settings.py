f7ab49441ddbf17e99f2c54d0f92a127e59aff69
Evan Ashbrook <Master@Evans-MacBook-Pro.local>
2021-06-20 18:34:34 -0400

    add db name back

--- mybackendapi/settings.py
+++ mybackendapi/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
 DATABASES['default'] = dj_database_url.config(
     conn_max_age=600, ssl_require=True)
 DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
-# DATABASES['default']['NAME'] = 'my_db'
+DATABASES['default']['NAME'] = 'my_db'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
